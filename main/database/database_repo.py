1dbf839b07e745c754537f2270ec0494efdde4a2
boldyrevn <boldyrev.now@mail.ru>
2023-07-08 02:15:52 +0400

    db.init is deleted

--- main/database/database_repo.py
+++ main/database/database_repo.py
@@ -6,42 +6,6 @@ class DataBase:
     def __init__(self, conn: asyncpg.Connection):
         self.conn = conn
 
-    async def init(self) -> None:
-        async with self.conn.transaction():
-            await self.conn.execute("""
-                CREATE TABLE IF NOT EXISTS merchants(
-                    id SERIAL PRIMARY KEY,
-                    name VARCHAR(64),
-                    username VARCHAR(64),
-                    type VARCHAR(16),
-                    description TEXT,
-                    in_search BOOLEAN DEFAULT TRUE,
-                    partner_id INTEGER
-                );
-                CREATE TABLE IF NOT EXISTS traders(
-                    id SERIAL PRIMARY KEY,
-                    name VARCHAR(64),
-                    username VARCHAR(64),
-                    description TEXT,
-                    in_search BOOLEAN DEFAULT TRUE,
-                    partner_id INTEGER
-                );
-                CREATE TABLE IF NOT EXISTS merchants_countries(
-                    merchant_id INTEGER,
-                    country VARCHAR(32),
-                    is_merged BOOLEAN DEFAULT FALSE,
-                    PRIMARY KEY(merchant_id, country),
-                    FOREIGN KEY(merchant_id) REFERENCES merchants(id) ON DELETE CASCADE
-                );
-                CREATE TABLE IF NOT EXISTS traders_countries(
-                    trader_id INTEGER,
-                    country VARCHAR(32),
-                    is_merged BOOLEAN DEFAULT FALSE,
-                    PRIMARY KEY(trader_id, country),
-                    FOREIGN KEY(trader_id) REFERENCES traders(id) ON DELETE CASCADE
-                );
-            """)
-
     async def close_connection(self):
         await self.conn.close()
 
